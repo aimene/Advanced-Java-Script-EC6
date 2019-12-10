22dc8d67b04a3e4f4dce4d1d862c7402772d7840
Aimene Amiour <aimene.amiour@uha.fr>
2019-12-10 15:48:49 +0100

    myChart diagram DONE

--- jsm1td56/temperatures.js
+++ jsm1td56/temperatures.js
@@ -1,18 +1,64 @@
 import * as view from './view.js'
 
+const ctx = document.getElementById('temp-canvas').getContext('2d');
+const table = document.getElementById('tempvilles');
+
+function temperaturesDatasetsFrom(temperatures , colors) {
+
+   let obj = new Array();
+   
+   let label ;
+   let data = new Array();;
+   let borderColor ;
+   let cpt =0 ;
+
+   
+
+   for (const temp of temperatures) {
+      label = temp.location;
+      data = temp.temperatures;
+      borderColor = colors[cpt];
+      cpt++;
+      obj.push({"label" : label, "data": data , "borderColor" : borderColor});
+   }
+
+   return obj ;
+   
+}
+
+function temperaturesChartFrom(t, ctx, col) {
+
+   let dataSet = temperaturesDatasetsFrom( t , col);
+   let myChart = new Chart(ctx, {
+       type:"line",
+       data:{
+       labels:view.temperaturesOfPage(table).months,
+       datasets: dataSet
+       },
+       options:{
+       title: { display: true, text: "description" },
+       datasets: { line: { lineTension:0, fill: false }},
+       responsive: false
+       }
+       });
+
+       return myChart;
+}
 
 
-
-function temperaturesFromTable() {
+function temperaturesFromTable(table) {
     
-   let obj = view.temperaturesOfPage();
-   console.log(obj);
+   let obj = view.temperaturesOfPage(table);
+   return obj ;
     
 }
 
 
 
 
-temperaturesFromTable();
+
+
+view.displayTemperaturesChart(temperaturesChartFrom(temperaturesFromTable(table).temperatures,ctx,view.newColors(8)));
+
 
 export { temperaturesFromTable}
--- jsm1td56/view.js
+++ jsm1td56/view.js
@@ -1,45 +1,64 @@
-const table = document.getElementById('tempvilles');
+//const color = new Array();
+let temperaturesChart = null;
 
-function temperaturesOfPage() { 
-    let description = getDescription(table);
-    let months = getMonths(table);
-    let temps = getLocationTemperaturesObject(table);
-   
-    let Object = {'description' :description,'months' : months, 'temperatures': temps};
-    
-    return Object;
+function displayTemperaturesChart(t) {
 
 
 }
-    function getDescription(table) {
-        let description = table.caption.textContent;
-        return description;
-    }
-    function getMonths(table) {
-        let monthsObject = table.tHead.getElementsByTagName('th');
-        let months = new Array;
-        for ( const month of monthsObject) {
-             months.push(month.textContent);
-        }
-        return months;
-    }
-    
-    function getLocationTemperaturesObject(table) {
-        let tBody = table.getElementsByTagName('tbody')[0];
-        let lines = tBody.getElementsByTagName('tr');
-        let temperatures = new Array;
-        for ( const line of lines) {
-         let tempsGroupValue;
-         let location;
-         let temps = new Array;
-         location= line.firstElementChild.textContent;
-         tempsGroupValue=line.getElementsByTagName('td');
-         for (const tempsValue of tempsGroupValue) { 
-             temps.push(tempsValue.textContent);
-         } 
-         temperatures.push({location : location,temperatures : temps});
-        }
-        return temperatures;
-    }
 
-export{ temperaturesOfPage}
+
+function newColors(n) {
+    let colorsTemp = new Array();
+    let H ;
+    let S  = 97; 
+    let L = 42 ;
+    for (let i = 0; i < n; i++) {
+        H = i*360/n;
+        colorsTemp.push("hsl("+H+","+ S+"%,"+L+"%)") ;
+    }
+    return colorsTemp;
+}
+
+
+function temperaturesOfPage(table) { 
+    let description = getDescription(table);
+    let months = getMonths(table);
+    let temps = getLocationTemperaturesObject(table);
+    let Object = {'description' :description,'months' : months, 'temperatures': temps};
+    return Object;
+}
+
+
+function getDescription(table) {     
+    let description = table.caption.textContent;
+    return description;
+}
+
+function getMonths(table) {
+    let monthsObject = table.tHead.getElementsByTagName('th');
+    let months = new Array;  
+    for ( const month of monthsObject) {
+            months.push(month.textContent);
+    }
+    return months;
+}
+
+function getLocationTemperaturesObject(table) {
+    let tBody = table.getElementsByTagName('tbody')[0];
+    let lines = tBody.getElementsByTagName('tr');
+    let temperatures = new Array;
+    for ( const line of lines) {
+        let tempsGroupValue;
+        let location;
+        let temps = new Array;
+        location= line.firstElementChild.textContent;
+        tempsGroupValue=line.getElementsByTagName('td');
+        for (const tempsValue of tempsGroupValue) { 
+            temps.push(tempsValue.textContent);
+        } 
+        temperatures.push({location : location,temperatures : temps});
+    }
+    return temperatures;
+}
+
+export{ temperaturesOfPage ,newColors,displayTemperaturesChart}
